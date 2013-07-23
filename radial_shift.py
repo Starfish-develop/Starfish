5f07904fc23b1e0f9cec527936d4957655a66ddb
Ian Czekala <iancze@gmail.com>
2013-07-22 21:44:09 -0400

    Merged radial_shift into downsample_model

--- radial_shift.py
+++ radial_shift.py
@@ -1,15 +1,9 @@
 import numpy as np
 
 
-def shift_vel(line, v):
-    '''Shift a line a certain velocity. Negative velocities refer to blueshift.'''
-    beta = (v*1e5)/c
-    return line * np.sqrt((1. + beta)/(1. - beta))
-    
-def gamma(v):
-    '''Calculates relativistic `\gamma` based upon velocity (in km/s).
-    ..math:
-        
-        \gamma = (1 - v_z^2/c^2)^{-1/2}'''
-    #Hasn't been vetted yet
-    return (1.0 - (v*1e5)**2/c**2)**(-0.5)
+
+@np.vectorize
+def shift_vz(lam_source, v):
+    '''Given the source wavelength, lam_sounce, return the observed wavelength based upon a velocity v in km/s. Negative velocities are towards the observer (blueshift).'''
+    lam_observe = lam_source * np.sqrt((c_kms + v)/(c_kms - v))
+    return lam_observe
