1345071bd31663ac7b58aedfa74e219d641cebb4
Ian Czekala <iancze@gmail.com>
2013-07-22 21:54:43 -0400

    Starting to refactor downsample_model

--- radial_shift.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-
-@np.vectorize
-def shift_vz(lam_source, v):
-    '''Given the source wavelength, lam_sounce, return the observed wavelength based upon a velocity v in km/s. Negative velocities are towards the observer (blueshift).'''
-    lam_observe = lam_source * np.sqrt((c_kms + v)/(c_kms - v))
-    return lam_observe
