129b50b8aba620edd56ab45929cdc478a150d59f
Ian Czekala <iancze@gmail.com>
2013-09-13 15:51:27 -0400

    Tightened parameters

--- plot_MCMC.py
+++ plot_MCMC.py
@@ -5,8 +5,14 @@ from numpy.polynomial import Chebyshev as Ch
 from echelle_io import rechellenpflat,load_masks
 from mpl_toolkits.axes_grid1 import ImageGrid
 from matplotlib.ticker import FormatStrFormatter as FSF
+import acor
 
-flatchain = np.load("output/flatchain.npy")
+chain = np.load("output/chain.npy")
+nwalkers = chain.shape[0]
+nsteps = chain.shape[1]
+#Give flatchain, too
+s = chain.shape
+flatchain = chain.reshape(s[0] * s[1], s[2])
 #flatchain = flatchain[80000:]
 lnchain = np.load("output/lnprobchain.npy")
 #lnchain = lnchain[80000:]
@@ -62,7 +68,7 @@ def hist_param(flatchain):
     
     axes[0].hist(flatchain[:,0],bins=40) #temp
     axes[1].hist(flatchain[:,1],bins=40) #logg
-    axes[2].hist(flatchain[:,2],bins=20,range=(40,50)) #vsini
+    axes[2].hist(flatchain[:,2],bins=40,range=(40,50)) #vsini
     axes[3].hist(flatchain[:,3],bins=50,range=(25,32)) #vz
     axes[4].hist(flatchain[:,4],bins=100) #c0
 
@@ -71,7 +77,7 @@ def hist_param(flatchain):
 
     fig.subplots_adjust(hspace=0.7,top=0.95,bottom=0.06)
     #plt.show()
-    plt.savefig('plots/hist_param.png')
+    plt.savefig('plots/hist_param_all.png')
 
 
 def joint_hist(p1,p2,**kwargs):
@@ -212,7 +218,7 @@ def staircase_plot_thesis(flatchain):
     N,M = flatchain.shape
 
     margin = 0.1
-    bins = [np.array([5350,5450,5550,5650,5750,5850,5950,6050,6150,6250,6350,6450]),np.array([1.75, 2.25, 2.75, 3.25, 3.75, 4.25, 4.75, 5.25]),np.linspace(43,52,num=40),np.linspace(26,31,num=40)] #,np.linspace(1.3e-28,2.5e-28,num=40),False,False]
+    bins = [np.linspace(5850,6100,num=30),np.linspace(3.2,3.9,num=30),np.linspace(43,52,num=30),np.linspace(26,30,num=30)] #,np.linspace(1.3e-28,2.5e-28,num=40),False,False]
 
     row_ax = []
     w = (1. - 2 * margin)/M
@@ -259,10 +265,14 @@ def staircase_plot_thesis(flatchain):
     row_ax[-1][0].set_xlabel(r"$T_{\rm eff}$")
     row_ax[-1][0].xaxis.set_major_formatter(FSF("%.0f"))
     row_ax[-1][1].set_xlabel(r"$\log g$")
+    row_ax[-1][1].xaxis.get_major_ticks()[0].label1On=False
     row_ax[-1][2].set_xlabel(r"$v \sin i$")
-
+    row_ax[-1][2].xaxis.get_major_ticks()[-1].label1On=False
+            
     #side labels
     row_ax[1][0].set_ylabel(r"$\log g$")
+    row_ax[1][0].yaxis.get_major_ticks()[-1].label1On=False
+    row_ax[1][0].yaxis.get_major_ticks()[0].label1On=False
     row_ax[2][0].set_ylabel(r"$v \sin i$")
     row_ax[3][0].set_ylabel(r"$v_z$")
     
@@ -278,12 +288,30 @@ def test_hist():
     ax.set_xlim(bins[0],bins[-1])
     plt.show()
 
-print(len(flatchain))
-#hist_param(flatchain[80000:])
-hist_param(flatchain[299000:])
+def plot_walker_position():
+    nwalkers = chain.shape[0]
+    nsteps = chain.shape[1]
+    steps = np.arange(nsteps)
+    for param in range(nparams):
+        fig = plt.figure(figsize=(11,11))
+        ax = fig.add_subplot(111)
+        for walker in range(nwalkers):
+            ax.plot(steps,chain[walker,:,param])
+        fig.savefig("plots/walkers/{:0>2.0f}.png".format(param))
+
+def get_acor():
+    for param in range(nparams):
+        print(acor.acor(chain[:,:,param]))
+
+
+#print(len(flatchain))
+#hist_param(flatchain[590000:])
+#hist_param(flatchain)
 #plot_random_data()
 #joint_hist(2,3,bins=[20,40],range=((50,65),(28,31)))
 #joint_hist(0,4,range=((),()))
 #draw_chebyshev_samples()
-#staircase_plot_thesis(flatchain)
+#staircase_plot_thesis(flatchain[590000:])
 #test_hist()
+#plot_walker_position()
+get_acor()
