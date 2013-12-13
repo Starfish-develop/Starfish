5fe468f30c79f9cbfb1344cc03a517cf5829dbe9
Ian Czekala <iancze@gmail.com>
2013-12-12 23:01:57 -0500

    Debugging grid

--- model.py
+++ model.py
@@ -60,8 +60,8 @@ grid_PHOENIX = {'T_points': np.array(
 grid_kurucz = {'T_points': np.arange(3500, 9751, 250),
                'logg_points': np.arange(1.0, 5.1, 0.5), 'Z_points': np.array([-0.5, 0.0, 0.5])}
 
-grid_BTSettl = {'T_points': [3000, 3100], 'logg_points': [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5],
-                'Z_points': [0.0]}
+grid_BTSettl = {'T_points': np.arange(3000, 7001, 100), 'logg_points': np.arange(1.0, 5.6, 0.5),
+                'Z_points': np.array([-0.5, 0.0, 0.5])}
 
 if config['grid'] == 'PHOENIX':
     grid = grid_PHOENIX
@@ -901,7 +901,8 @@ def main():
     #                                        1.06044794e+00 ,  1.05605767e+00])))
     #print(lnprob_lognormal_marg(np.array([6.36775928e+03 ,  4.15686725e+00 , -1.39802799e-01 ,  7.97754533e+00,
     #                                         6.85987877e+01 , 0.0, 1e-15 ,  1.00423485e+00, 1, 1])))
-    fls = model(wls, 7000, 4.2, 0.0, 4.0, 0.0, 1e-15)
+    fls = model(wls, 4900, 3.5, 0.0, 4.0, 0.0, 1e-15)
+    print(fls)
 
     import matplotlib.pyplot as plt
     plt.plot(wls[1],fls[1])
