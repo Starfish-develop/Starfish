86848607a278cca212d98914b108e1400e58f6a3
Ian Czekala <iancze@gmail.com>
2014-01-30 01:19:14 -0500

    reduced to 20 processes

--- scripts/create_master_grid.py
+++ scripts/create_master_grid.py
@@ -1,12 +1,12 @@
 from StellarSpectra import grid_tools
 
-raw_library_path = "/n/home07/iczekala/holyscratch/raw_libraries/PHOENIX/"
+raw_library_path = "/n/holyscratch/panstarrs/iczekala/raw_libraries/PHOENIX/"
 
 mygrid = grid_tools.PHOENIXGridInterface(air=True, norm=True, base=raw_library_path)
 spec = mygrid.load_file({"temp":5000, "logg":3.5, "Z":0.0,"alpha":0.0})
 wldict = spec.calculate_log_lam_grid()
 
-out_path = "/n/home07/iczekala/holyscratch/master_grids/" + "PHOENIX_master.hdf5"
+out_path = "/n/holyscratch/panstarrs/iczekala/master_grids/" + "PHOENIX_master.hdf5"
 
 HDF5Creator = grid_tools.HDF5GridCreator(mygrid, filename=out_path, wldict=wldict)
 HDF5Creator.process_grid()
