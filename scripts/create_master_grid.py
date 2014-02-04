b327917373d8e6713d951306f7db383003e00a37
Ian Czekala <iancze@gmail.com>
2014-02-04 10:31:48 -0500

    now 10 processes

--- scripts/create_master_grid.py
+++ scripts/create_master_grid.py
@@ -11,7 +11,7 @@ wldict = spec.calculate_log_lam_grid()
 
 #out_path = "/n/holyscratch/panstarrs/iczekala/master_grids/" + "PHOENIX_master.hdf5"
 out_path = "/scratch/" + "PHOENIX_master.hdf5"
-HDF5Creator = grid_tools.HDF5GridCreator(mygrid, filename=out_path, wldict=wldict, nprocesses=6, chunksize=1)
+HDF5Creator = grid_tools.HDF5GridCreator(mygrid, filename=out_path, wldict=wldict, nprocesses=10, chunksize=1)
 
 HDF5Creator.process_grid()
 
