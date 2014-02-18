e3081908422d82380b757886641e5a372f0b53a9
Ian Czekala <iancze@gmail.com>
2014-02-18 03:17:10 -0500

    playing around with sparse matrices

--- class_parallel.py
+++ class_parallel.py
@@ -1,106 +1,25 @@
 import multiprocessing as mp
 import numpy as np
 import os
-#
-#def info(title):
-#    print(title)
-#    print( 'module name:', __name__)
-#    if hasattr(os, 'getppid'):  # only available on Unix
-#        print( 'parent process:', os.getppid())
-#    print( 'process id:', os.getpid())
-#
-#def f(name):
-#    info('function f')
-#    print('hello', name)
-#
-#if __name__ == '__main__':
-#    info('main line')
-#    p = mp.Process(target=f, args=('bob',))
-#    p.start()
-#    p.join()
+import h5py
 
+class Model:
+    def __init__(self):
+        self.f = h5py.File('libraries/PHOENIX_test.hdf5', "w")
+        pass
 
-#from multiprocessing import Process, Queue
-#
-#def f(q):
-#    q.put([42, None, 'hello'])
-#
-#if __name__ == '__main__':
-#    q = Queue()
-#    p = Process(target=f, args=(q,))
-#    p.start()
-#    print(q.get())    # prints "[42, None, 'hello']"
-#    p.join()
-#class Parallel:
-#    def __init__(self):
-#        #self.pool = mp.Pool(4)
-#        #self.process = process
-#        pass
-#
-#    def process(self, parameters):
-#        print(parameters)
-#
-#    def do_parallel(self):
-#        pass
-#
-#def f(args):
-#    print(args)
+    def __call__(self, params):
+        return params
 
-#p = mp.Process(target=f, args=('bob',))
-#p.start()
-#p.join()
+myModel = Model()
 
+def lnprob(param):
+    result = myModel(param)
+    print(result)
 
-import time
-import multiprocessing as mp
+pool = mp.Pool(mp.cpu_count())
+list(pool.map(lnprob, np.arange(20)))
 
-import numpy as np
-
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-
-
-class AsyncPlotter():
-
-    def __init__(self, processes=mp.cpu_count()):
-        self.processes = processes
-        self.pids = []
-        self.pid = os.getpid()
-        self.sqrt = np.sqrt
-
-        #Stuff queue
-
-
-    def async_plotter(self, fig, filename):
-        fig.savefig(filename)
-        plt.close(fig)
-
-
-    def join(self):
-        for i in range(10):
-            # Generate random points
-            x = np.random.random(10000)
-            y = np.random.random(10000)
-
-            # Generate figure
-            fig = plt.figure()
-            ax = fig.add_subplot(1, 1, 1)
-            ax.set_xscale('log')
-            ax.set_yscale('log')
-            ax.scatter(x, y)
-
-            # Add figure to queue
-            p = mp.Process(target=self.async_plotter, args=(fig, '%04i.png' % i))
-            p.start()
-            self.pids.append(p)
-
-        for p in self.pids:
-            p.join()
-
-# Create instance of Asynchronous plotter
-a = AsyncPlotter()
-a.join()
 
 
 #Class parallel methods will work
