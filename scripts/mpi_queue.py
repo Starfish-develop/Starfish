3085513ad343bf67a89497edcb281b1baffa2808
Ian Czekala <iancze@gmail.com>
2014-02-22 08:47:05 -0500

    Deleted old files, moved older scripts to /scripts

--- scripts/mpi_queue.py
+++ scripts/mpi_queue.py
@@ -2,25 +2,10 @@ from mpi4py import MPI
 import numpy as np
 from itertools import zip_longest
 
-#How to determine total number of processes?
-
-#self.comm = MPI.COMM_WORLD if comm is None else comm
-#self.rank = self.comm.Get_rank()
-#self.size = self.comm.Get_size() - 1
-
 indexes = [i for i in range(233)]
 
 rank = MPI.COMM_WORLD.rank  # The process ID
 nprocesses = MPI.COMM_WORLD.size
-
-#Divide up indexes into chunks based on size
-#length = len(indexes)
-
-#print(length//size)
-
-#I have a list of "length", and "nprocesses" to deal with it. I want a new list of length nprocesses.
-#print(np.ceil(length/size))
-
 
 def chunk(mylist, n=nprocesses):
     length = len(mylist)
