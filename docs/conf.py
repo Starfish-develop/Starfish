2295b9c7f3759fbe4dd0648c969d8e4b5924a92a
Ian Czekala <iancze@gmail.com>
2014-02-22 20:33:02 -0500

    added skeleton documentation for grid_tools

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,8 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
+#sys.path.insert(0, os.path.abspath('grid_tools.py'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -26,7 +28,8 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath',
+              'sphinx.ext.mathjax', 'sphinx.ext.viewcode', 'sphinx.ext.graphviz', 'sphinx.ext.inheritance_diagram']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
---
+++ docs/index.rst
@@ -0,0 +1,27 @@
+StellarSpectra documentation
+============================
+
+StellarSpectra is designed to infer stellar properties such as effective temperature :math:`T_{\rm eff}`, surface
+gravity :math:`\log(g)`, and metallicity :math:`[{\rm Fe}/{\rm H}]` from high resolution spectra using a Bayesian model.
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   grid_tools
+   model
+
+.. math::
+
+   \pi_T(x) = \left[ l(x) \right]^{\frac{1}{T}} p(x)
+
+Copyright Ian Czekala 2013, 2014
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
