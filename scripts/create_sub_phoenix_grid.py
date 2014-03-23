dd9200343ed1dcf30159f019635ba31119ce57c2
Ian Czekala <iancze@gmail.com>
2014-03-23 08:55:35 -0400

    script error

--- scripts/create_sub_phoenix_grid.py
+++ scripts/create_sub_phoenix_grid.py
@@ -7,7 +7,7 @@ raw_library_path = "/n/holyscratch/panstarrs/iczekala/raw_libraries/PHOENIX/"
 mygrid = grid_tools.PHOENIXGridInterface(base=raw_library_path)
 
 out_path = "/scratch/" + "PHOENIX_submaster.hdf5"
-HDF5Stuffer = grid_tools.HDF5GridCreator(mygrid, filename=out_path,
+HDF5Stuffer = grid_tools.HDF5GridStuffer(mygrid, filename=out_path,
                         ranges={"temp":(5000, 7000), "logg":(3.5,5.5), "Z":(-1.0,0.0), "alpha":(0.0,0.4)})
 
 HDF5Stuffer.process_grid()
