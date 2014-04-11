8aad44ec9308afafd2297fa8051f317dd65016c2
Ian Czekala <iancze@gmail.com>
2014-04-11 16:14:58 -0400

    Test emcee running as MH

--- StellarSpectra/tests/test_covariance.py
+++ StellarSpectra/tests/test_covariance.py
@@ -8,6 +8,7 @@ class TestCCovarianceMatrix:
     def setup_class(self):
         self.dataspectrum = DataSpectrum.open("tests/WASP14/WASP-14_2009-06-15_04h13m57s_cb.spec.flux", orders=np.array([21,22,23]))
         self.dataspectrum.fls = np.ones_like(self.dataspectrum.wls)
+        #self.dataspectrum.sigmas = 2*np.ones_like(self.dataspectrum.wls)
         print(self.dataspectrum.fls)
         self.flux = flux = np.zeros((2298,)) #so that the residuals == 1.0
         self.CovarianceMatrix = CovarianceMatrix(self.dataspectrum, order_index=1)
@@ -16,10 +17,17 @@ class TestCCovarianceMatrix:
         print("Initialized OK")
 
     def test_update_global(self):
-        self.CovarianceMatrix.update_global({"sigAmp":1, "logAmp":0, "l":1})
+        self.CovarianceMatrix.update_global({"sigAmp":1, "logAmp":0.0, "l":1})
+        # S = self.CovarianceMatrix.cholmod_to_scipy_sparse()
+        # print("Value of 100, 100 is ", S[100, 100])
+
 
     def test_evaluate(self):
         print("\n NO REGION lnprob = ", self.CovarianceMatrix.evaluate(self.flux))
+
+    def test_get_region_coverage_once(self):
+        cov = self.CovarianceMatrix.get_region_coverage()
+        print(np.sum(cov))
 
     def test_create_region(self):
         self.CovarianceMatrix.create_region({"h":1.0, "a":1.0, "mu":5180., "sigma":1.0})
@@ -47,8 +55,24 @@ class TestCCovarianceMatrix:
         print("\n updated TWO REGION lnprob = ", self.CovarianceMatrix.evaluate(self.flux))
 
     def test_update_second_region(self):
-        self.CovarianceMatrix.update_region(1, {"h":1.0, "a":0.0, "mu":5200., "sigma":1.0})
+        self.CovarianceMatrix.update_region(1, {"h":1.0, "a":1.0, "mu":5200., "sigma":1.0})
         print(self.CovarianceMatrix.print_all_regions())
         print("\n updated TWO REGION lnprob = ", self.CovarianceMatrix.evaluate(self.flux))
+
+    def test_get_region_coverage_twice(self):
+        cov = self.CovarianceMatrix.get_region_coverage()
+        print(np.sum(cov))
+
+    # def test_cholmod_to_scipy_sparse(self):
+    #     S = self.CovarianceMatrix.cholmod_to_scipy_sparse()
+    #     print("Value of 100, 100 is ", S[100, 100])
+    #     import matplotlib.pyplot as plt
+    #     # plt.spy(S, markersize=0.001)
+    #     plt.imshow(S.todense(), origin='upper', interpolation='none')
+    #     plt.show()
+
+
+    def test_common_equal(self):
+        self.CovarianceMatrix.test_common_equal()
 
 #Create_region will really fuck up if mu < wl[0]. How to circumvent this?
--- StellarSpectra/tests/test_emcee.py
+++ StellarSpectra/tests/test_emcee.py
@@ -1,92 +1,33 @@
-#Is it possible to update lnprob args without clearing all of the samples?
+#Use emcee as a Metropolis-Hastings so we can avoid a lot of the difficulty of the ensemble sampler for the moment.
 
 import numpy as np
 import emcee
-from emcee.utils import MPIPool
-import sys
-
-
-class function_wrapper(object):
-    """
-    This is a hack of a hack to make the likelihood function pickleable and update-able when ``args``
-    or ``kwargs`` are also included.
-
-    """
-    def __init__(self, f, args, kwargs):
-        self.f = f
-        self.args = args
-        self.kwargs = kwargs
-
-    def __call__(self, x):
-        try:
-            return self.f(x, *self.args, **self.kwargs)
-        except:
-            import traceback
-            print("emcee: Exception while calling your likelihood function:")
-            print("  params:", x)
-            print("  args:", self.args)
-            print("  kwargs:", self.kwargs)
-            print("  exception:")
-            traceback.print_exc()
-            raise
-
-def update_args(sampler, *args, **kwargs):
-    '''
-    Take a sampler object and update it's args.
-    '''
-    sampler.args = args
-    sampler.kwargs = kwargs
-    sampler.lnprobfn = function_wrapper(lnprob, args=args, kwargs=kwargs)
 
 
 
-xs = np.linspace(0, 10, num=20)
-ys = 5 + 10 * xs + np.random.normal(size=20)
+#create our lnprob as a multidimensional Gaussian, where icov is C^{-1}
+def lnprob(x, mu, icov):
+    diff = x-mu
+    return -np.dot(diff,np.dot(icov,diff))/2.0
 
-def lnprob(p, b):
-    m = p[0]
-    chi2 = np.sum((ys - (b + m*xs))**2)
-    #print("{:.1f} ".format(b),end="")
-    return -0.5 * chi2
+ndim = 2
 
-pool = MPIPool()
+#Choose some random mean for these points
+means = np.random.rand(ndim)
 
-if not pool.is_master():
-#   Wait for instructions from the master process.
-    pool.wait() #all execution takes place here
-    sys.exit(0) #this happens at the very end of the run.
+#This creates a symmetric covariance matrix
+cov = 0.5 - np.random.rand(ndim ** 2).reshape((ndim, ndim)) #create C
+cov = np.triu(cov)
+cov += cov.T - np.diag(cov.diagonal())
+cov = np.dot(cov,cov)
 
-nwalkers = 10
-burn_in = 10
-ndim = 1
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=np.array([-10]))#, pool=pool)
+#This takes the inverse
+icov = np.linalg.inv(cov)
 
-#Declare starting indexes
-m = np.random.uniform(low=8, high=12, size=(nwalkers,))
-p0 = np.array([m]).T
-pos, prob, state = sampler.run_mcmc(p0, burn_in)
-sampler.reset()
-pos, prob, state = sampler.run_mcmc(pos, 1000, rstate0=state)
-print(np.mean(sampler.flatchain))
+MH_cov = np.array([[0.1, 0],[0., 0.1]])
 
+sampler = emcee.MHSampler(MH_cov, ndim, lnprob, args=[means, icov])
 
+sampler.run_mcmc(np.array([0, 0]), 100)
 
-#Try updating args
-update_args(sampler, 5)
-pos, prob, state = sampler.run_mcmc(pos, 1000, rstate0=state)
-#Burn in again
-sampler.reset()
-pos, prob, state = sampler.run_mcmc(pos, 1000, rstate0=state)
-print(np.mean(sampler.flatchain))
-
-
-pool.close()
-
-##How to use ProgressBar
-#from progressbar import ProgressBar, Percentage, Bar, ETA
-#pbar = ProgressBar(widgets=[Percentage(), Bar(), ETA()], maxval=1000).start()
-#index = 0
-#for result in sampler.sample(pos, prob, state, iterations=1000):
-#    index += 1
-#    pbar.update(index)
-#pbar.finish()
+print(sampler.flatchain)
