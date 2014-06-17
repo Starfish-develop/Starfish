3dc73ed930beeb5308d2a81383d903fb9f788422
Ian Czekala <iancze@gmail.com>
2014-06-17 03:49:28 -0400

    more updates on presentation

--- scripts/HDF5/create_obj_grid.py
+++ scripts/HDF5/create_obj_grid.py
@@ -6,17 +6,14 @@ import StellarSpectra.constants as C
 #scout
 # raw_library_path = "libraries/raw/PHOENIX/"
 
-myHDF5Interface = grid_tools.HDF5Interface("libraries/PHOENIX_submaster.hdf5")
-myInstrument = grid_tools.TRES()
+myHDF5Interface = grid_tools.HDF5Interface("libraries/PHOENIX_M.hdf5")
+myInstrument = grid_tools.SPEX()
 
 # out_path = "/scratch/" + "PHOENIX_LkCa15.hdf5"
 # out_path = "libraries/" + "PHOENIX_LkCa15.hdf5"
-out_path = "libraries/" + "PHOENIX_objgrid.hdf5"
+out_path = "libraries/" + "PHOENIX_SPEX_M.hdf5"
 
 HDF5ObjGridCreator = grid_tools.HDF5ObjGridCreator(myHDF5Interface, filename=out_path, Instrument=myInstrument,
-                        ranges={"temp":(5500, 6500), "logg":(4.0,5.0), "Z":(-0.5,0.0), "alpha":(0.0,0.0)})
-
-# print(HDF5ObjGridCreator.process_flux({"temp":5100, "logg":4.5, "Z":0.0, "alpha":0.0}))
-print(HDF5ObjGridCreator.points)
+                        ranges={"temp":(2300, 2400), "logg":(4.5,5.0), "Z":(-0.5,0.0), "alpha":(0.0,0.0)})
 
 HDF5ObjGridCreator.process_grid()
