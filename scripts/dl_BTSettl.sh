803deaf7ad717ee295b1a2ca848b936fc4689df8
Ian Czekala <iancze@gmail.com>
2014-06-22 21:23:39 -0400

    added dl_BTSettl

---
+++ scripts/dl_BTSettl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -bc http://phoenix.ens-lyon.fr/Grids/BT-Settl/AGSS2009/SPECTRA/BT-Settl_M+0.3_a+0.0_hot.tar
+wget -bc http://phoenix.ens-lyon.fr/Grids/BT-Settl/AGSS2009/SPECTRA/BT-Settl_M+0.5_a+0.0_hot.tar
+wget -bc http://phoenix.ens-lyon.fr/Grids/BT-Settl/AGSS2009/SPECTRA/BT-Settl_M-0.0_a+0.0_hot.tar
+wget -bc http://phoenix.ens-lyon.fr/Grids/BT-Settl/AGSS2009/SPECTRA/BT-Settl_M-0.0_cool.tar
+wget -bc http://phoenix.ens-lyon.fr/Grids/BT-Settl/AGSS2009/SPECTRA/BT-Settl_M-0.5_a+0.0_hot.tar
+wget -bc http://phoenix.ens-lyon.fr/Grids/BT-Settl/AGSS2009/SPECTRA/BT-Settl_M-1.0_a+0.2_hot.tar
