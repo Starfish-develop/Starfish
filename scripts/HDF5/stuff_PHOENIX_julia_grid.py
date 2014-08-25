7fbf1144dc65a0fc29cf2a316805d96e6ecf6ad3
Ian Czekala <iancze@gmail.com>
2014-08-25 14:51:35 -0400

    smaller grid

--- scripts/HDF5/stuff_PHOENIX_julia_grid.py
+++ scripts/HDF5/stuff_PHOENIX_julia_grid.py
@@ -5,7 +5,7 @@ import StellarSpectra.constants as C
 
 raw_library_path = "/pool/scout0/libraries/raw/PHOENIX/"
 
-mygrid = grid_tools.PHOENIXGridInterface(base=raw_library_path, wl_range=(3000, 10000))
+mygrid = grid_tools.PHOENIXGridInterface(base=raw_library_path, wl_range=(4900, 7000))
 
 out_path = "libraries/PHOENIX_F_julia_hires.hdf5"
 HDF5Stuffer = grid_tools.HDF5GridStuffer(mygrid, filename=out_path, ranges={"temp":(5600, 6600), "logg":(2.5, 6.0),
