6170af3a8d8edaa290c74cca1c423280db2aefa8
Ian Czekala <iancze@gmail.com>
2014-08-25 02:08:23 -0400

    Seems like it's done

--- hdfplot.py
+++ hdfplot.py
@@ -8,4 +8,8 @@ import hdfutils
 Script designed to plot the HDF5 output from MCMC runs. It will only plot the first chain, to prevent overwriting.
 '''
 
+if hdfutils.args.cov:
+    hdfutils.estimate_covariance(hdfutils.flatchainTreeList[0])
+
 hdfutils.plot(hdfutils.flatchainTreeList[0], clip_stellar=hdfutils.args.stellar_params)
+
