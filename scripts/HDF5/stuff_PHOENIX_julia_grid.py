3246c131459cfd6cb9b075ffe0a330504f96ffc3
Ian Czekala <iancze@gmail.com>
2014-08-27 00:58:49 -0400

    stuff grid

--- scripts/HDF5/stuff_PHOENIX_julia_grid.py
+++ scripts/HDF5/stuff_PHOENIX_julia_grid.py
@@ -9,7 +9,7 @@ mygrid = grid_tools.PHOENIXGridInterface(base=raw_library_path, wl_range=(4900, 
 
 out_path = "libraries/PHOENIX_F_julia_hires.hdf5"
 HDF5Stuffer = grid_tools.HDF5GridStuffer(mygrid, filename=out_path, ranges={"temp":(5600, 6600), "logg":(2.5, 6.0),
-                                                                            "Z":(-1.5, 1.0), "alpha":(0.0, 0.0)})
+                                                                            "Z":(-2.0, 1.0), "alpha":(0.0, 0.0)})
 
 HDF5Stuffer.process_grid()
 
