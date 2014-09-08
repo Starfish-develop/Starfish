c3de7f35c18e90064b83538fdc49ce079a3553cd
Ian Czekala <iancze@gmail.com>
2014-09-08 18:48:43 -0400

    Making Kurucz grid2

--- scripts/HDF5/create_error_spec.py
+++ scripts/HDF5/create_error_spec.py
@@ -1,6 +1,7 @@
 from StellarSpectra import grid_tools
 
-interface = grid_tools.HDF5Interface("libraries/PHOENIX_TRES_F.hdf5")
+#interface = grid_tools.HDF5Interface("libraries/PHOENIX_TRES_F.hdf5")
+interface = grid_tools.HDF5Interface("libraries/Kurucz_TRES.hdf5")
 
 machine = grid_tools.ErrorSpectrumCreator(interface)
 machine.process_grid()
