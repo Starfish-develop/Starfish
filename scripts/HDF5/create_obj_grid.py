057e7d7ac51f7dc82960c0bfaafa91902f678bcc
Ian Czekala <iancze@gmail.com>
2014-09-08 18:48:22 -0400

    Making Kurucz grid

--- scripts/HDF5/create_obj_grid.py
+++ scripts/HDF5/create_obj_grid.py
@@ -4,9 +4,9 @@ from StellarSpectra import grid_tools
 # raw_library_path = "/n/holyscratch/panstarrs/iczekala/raw_libraries/PHOENIX/"
 # out_path = "/scratch/" + "PHOENIX_LkCa15.hdf5"
 
-#myHDF5Interface = grid_tools.HDF5Interface("libraries/Kurucz_master.hdf5")
+myHDF5Interface = grid_tools.HDF5Interface("libraries/Kurucz_master.hdf5")
 #myHDF5Interface = grid_tools.HDF5Interface("libraries/PHOENIX_F_julia.hdf5")
-myHDF5Interface = grid_tools.HDF5Interface("libraries/PHOENIX_F.hdf5")
+#myHDF5Interface = grid_tools.HDF5Interface("libraries/PHOENIX_F.hdf5")
 # myHDF5Interface = grid_tools.HDF5Interface("libraries/PHOENIX_LkCa15.hdf5")
 # myHDF5Interface = grid_tools.HDF5Interface("libraries/PHOENIX_M.hdf5")
 
@@ -16,15 +16,15 @@ myInstrument = grid_tools.TRES()
 # myInstrument = grid_tools.SPEX_SXD()
 # myInstrument = grid_tools.SPEXMgb()
 
-out_path = "libraries/" + "PHOENIX_TRES_F.hdf5"
+#out_path = "libraries/" + "PHOENIX_TRES_F.hdf5"
 # out_path = "libraries/" + "PHOENIX_TRES_4000.hdf5"
 # out_path = "libraries/" + "PHOENIX_TRES_2300.hdf5"
 
-#out_path = "libraries/" + "Kurucz_TRES.hdf5"
+out_path = "libraries/" + "Kurucz_TRES.hdf5"
 
 #out_path = "libraries/" + "PHOENIX_TRES_F_julia.hdf5"
 
-HDF5InstGridCreator = grid_tools.HDF5InstGridCreator(myHDF5Interface, filename=out_path, Instrument=myInstrument,
-                        ranges={"temp":(5400, 6700), "logg":(2.5,5.5), "Z":(-1.5,1.), "alpha":(0.0,0.0)})
+HDF5InstGridCreator = grid_tools.HDF5InstGridCreator(myHDF5Interface, filename=out_path, Instrument=myInstrument)#,
+                        #ranges={"temp":(5400, 6700), "logg":(2.5,5.5), "Z":(-1.5,1.), "alpha":(0.0,0.0)})
 
 HDF5InstGridCreator.process_grid()
