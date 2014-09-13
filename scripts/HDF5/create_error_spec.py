9767f17cd0754d88e465287ca9abc527ecd99114
Ian Czekala <iancze@gmail.com>
2014-09-13 17:59:50 -0400

    stuffing Gl51 grid

--- scripts/HDF5/create_error_spec.py
+++ scripts/HDF5/create_error_spec.py
@@ -1,7 +1,8 @@
 from StellarSpectra import grid_tools
 
 #interface = grid_tools.HDF5Interface("libraries/PHOENIX_TRES_F.hdf5")
-interface = grid_tools.HDF5Interface("libraries/Kurucz_TRES.hdf5")
+#interface = grid_tools.HDF5Interface("libraries/Kurucz_TRES.hdf5")
+interface = grid_tools.HDF5Interface("libraries/PHOENIX_SPEX_M.hdf5")
 
 machine = grid_tools.ErrorSpectrumCreator(interface)
 machine.process_grid()
