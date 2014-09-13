74af8404479ba52b9904b5d06c135851c475a6ef
Ian Czekala <iancze@gmail.com>
2014-09-13 18:17:17 -0400

    ready to rpocess PHOENIX grid too

--- scripts/HDF5/create_sub_phoenix_grid.py
+++ scripts/HDF5/create_sub_phoenix_grid.py
@@ -5,11 +5,11 @@ raw_library_path = "libraries/raw/PHOENIX/"
 
 mygrid = grid_tools.PHOENIXGridInterface(base=raw_library_path)
 
-out_path = "libraries/" + "PHOENIX_M.hdf5"
-#out_path = "libraries/" + "PHOENIX_F.hdf5"
+#out_path = "libraries/" + "PHOENIX_M.hdf5"
+out_path = "libraries/" + "PHOENIX_F.hdf5"
 
 
 HDF5Stuffer = grid_tools.HDF5GridStuffer(mygrid, filename=out_path,
-                        ranges={"temp":(2300, 4200), "logg":(4.0,6.0), "Z":(-1.0,1.0), "alpha":(0.0,0.0)})
+                        ranges={"temp":(5300, 6600), "logg":(2.5,5.0), "Z":(-1.5,0.5), "alpha":(0.0,0.0)})
 
 HDF5Stuffer.process_grid()
