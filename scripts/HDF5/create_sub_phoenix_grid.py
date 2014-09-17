5048963ff0f5d93d82ebdaf1327a0e75b0d667fb
Ian Czekala <iancze@gmail.com>
2014-09-16 23:19:40 -0400

    Making Julia grid

--- scripts/HDF5/create_sub_phoenix_grid.py
+++ scripts/HDF5/create_sub_phoenix_grid.py
@@ -5,11 +5,11 @@ raw_library_path = "libraries/raw/PHOENIX/"
 
 mygrid = grid_tools.PHOENIXGridInterface(base=raw_library_path)
 
-#out_path = "libraries/" + "PHOENIX_M.hdf5"
-out_path = "libraries/" + "PHOENIX_F.hdf5"
+out_path = "libraries/" + "PHOENIX_M_julia_hires.hdf5"
+#out_path = "libraries/" + "PHOENIX_F.hdf5"
 
 
 HDF5Stuffer = grid_tools.HDF5GridStuffer(mygrid, filename=out_path,
-                        ranges={"temp":(5300, 6600), "logg":(2.5,5.0), "Z":(-1.5,0.5), "alpha":(0.0,0.0)})
+                        ranges={"temp":(2500, 3800), "logg":(3.5,6.0), "Z":(-1.5,1.0), "alpha":(0.0,0.0)})
 
 HDF5Stuffer.process_grid()
