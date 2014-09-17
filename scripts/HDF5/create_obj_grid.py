df829c83b5304ba8b03a4cea4e45c7add2f6586b
Ian Czekala <iancze@gmail.com>
2014-09-16 23:58:40 -0400

    spexify julia grid

--- scripts/HDF5/create_obj_grid.py
+++ scripts/HDF5/create_obj_grid.py
@@ -8,7 +8,8 @@ from StellarSpectra import grid_tools
 #myHDF5Interface = grid_tools.HDF5Interface("libraries/PHOENIX_F_julia.hdf5")
 #myHDF5Interface = grid_tools.HDF5Interface("libraries/PHOENIX_F.hdf5")
 # myHDF5Interface = grid_tools.HDF5Interface("libraries/PHOENIX_LkCa15.hdf5")
-myHDF5Interface = grid_tools.HDF5Interface("libraries/PHOENIX_M.hdf5")
+#myHDF5Interface = grid_tools.HDF5Interface("libraries/PHOENIX_M.hdf5")
+myHDF5Interface = grid_tools.HDF5Interface("libraries/PHOENIX_M_interp.hdf5")
 
 print("Library only spans from", myHDF5Interface.bounds)
 
@@ -19,7 +20,8 @@ myInstrument = grid_tools.SPEX()
 # out_path = "libraries/" + "PHOENIX_TRES_F.hdf5"
 # out_path = "libraries/" + "Kurucz_TRES.hdf5"
 
-out_path = "libraries/" + "PHOENIX_SPEX_M.hdf5"
+#out_path = "libraries/" + "PHOENIX_SPEX_M.hdf5"
+out_path = "libraries/" + "PHOENIX_SPEX_M_interp.hdf5"
 
 #out_path = "libraries/" + "PHOENIX_TRES_F_julia.hdf5"
 
