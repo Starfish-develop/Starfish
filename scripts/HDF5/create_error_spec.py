5d7fca7e6113e656847c7fee765323bfb188b1ac
Ian Czekala <iancze@gmail.com>
2014-09-17 00:45:51 -0400

    using truncated grid

--- scripts/HDF5/create_error_spec.py
+++ scripts/HDF5/create_error_spec.py
@@ -2,7 +2,7 @@ from StellarSpectra import grid_tools
 
 #interface = grid_tools.HDF5Interface("libraries/PHOENIX_TRES_F.hdf5")
 #interface = grid_tools.HDF5Interface("libraries/Kurucz_TRES.hdf5")
-interface = grid_tools.HDF5Interface("libraries/PHOENIX_SPEX_M.hdf5")
+interface = grid_tools.HDF5Interface("libraries/PHOENIX_SPEX_M_coarse.hdf5")
 
 machine = grid_tools.ErrorSpectrumCreator(interface)
 machine.process_grid()
