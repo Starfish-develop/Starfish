99e49dbc8122c5256d09f89a62806ca42104bd3e
Ian Czekala <iancze@gmail.com>
2014-10-07 17:39:27 -0400

    starting PHOENIX run

--- scripts/emulator/add_samples.py
+++ scripts/emulator/add_samples.py
@@ -6,6 +6,8 @@ import argparse
 parser = argparse.ArgumentParser(prog="add_samples.py",
                 description="Decompose the spectra into eigenspectra.")
 parser.add_argument("input", help="*.yaml file specifying parameters.")
+parser.add_argument("--params", action="store_true", help="Store optimized parameters instead"
+                    "of samples parameters.")
 args = parser.parse_args()
 
 import yaml
@@ -21,9 +23,16 @@ ncomp = cfg["ncomp"]
 
 #Load individual samples and then concatenate them
 base = cfg["outdir"]
-samples = np.array([np.load(base + "samples_w{}.npy".format(i)) for i in range(ncomp)])
 
 hdf5 = h5py.File(filename, "r+")
-sdset = hdf5.create_dataset("samples", samples.shape, compression='gzip', compression_opts=9)
-sdset[:] = samples
+
+if args.params:
+    params = np.load(base + "params.npy")
+    pdset = hdf5.create_dataset("params", params.shape, compression="gzip", compression_opts=9)
+    pdset[:] = params
+else:
+    samples = np.array([np.load(base + "samples_w{}.npy".format(i)) for i in range(ncomp)])
+    sdset = hdf5.create_dataset("samples", samples.shape, compression='gzip', compression_opts=9)
+    sdset[:] = samples
+
 hdf5.close()
