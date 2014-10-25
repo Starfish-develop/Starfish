bdcdcf7d161b745ebe5800243eaa78e1272c781d
Ian Czekala <iancze@gmail.com>
2014-10-24 20:26:33 -0400

    Cleaned up a lot of extraneous files

--- Starfish/em_cov.pyx
+++ Starfish/em_cov.pyx
@@ -1,4 +1,3 @@
-import scipy.sparse as sp
 import numpy as np
 import math
 cimport numpy as np
@@ -50,37 +49,6 @@ def sigma(np.ndarray[np.double_t, ndim=2] gparams, double a2, double lt2, double
 
     return mat
 
-
-#@cython.boundscheck(False)
-cdef csigma(int m, iprecision, irhos, sparams):
-    '''
-    Create the dense matrix using cython
-    '''
-    cdef int i = 0
-    cdef int j = 0
-    cdef double cov = 0.0
-    lnrhos = np.log(irhos)
-    cdef np.ndarray[np.double_t, ndim=2] mat = np.empty((m,m), dtype=np.float64)
-    for i in range(m):
-        for j in range(i+1):
-                cov = R(sparams[i], sparams[j], lnrhos)
-                mat[i,j] = cov
-                mat[j,i] = cov
-    return iprecision*mat
-
-def Sigma(sparams, precisions, rhos):
-    '''
-    Create all Sigmas
-    '''
-    m = len(sparams)
-
-    sigmas = []
-
-    for iprecision, irhos in zip(precisions, rhos):
-        sigmas.append(csigma(m, iprecision, irhos, sparams))
-
-    return sp.block_diag(sigmas)
-
 def V12_w(params, np.ndarray[np.double_t, ndim=2] gparams, double a2, double lt2, double ll2, double lz2):
     '''
     Create V12, but just for a single weight.
@@ -119,45 +87,4 @@ def V22_w(params, double a2, double lt2, double ll2, double lz2):
         for j in range(npoints):
             mat[i,j] = k(params[i], params[j], a2, lt2, ll2, lz2)
     return mat
-
-def V12(params, sparams, rhos):
-    '''
-    Given the new parameters and the set parameters, setup the covariance matrix.
-    '''
-    m = len(sparams)
-    cdef int ncomp = len(rhos)
-    nparams = len(params)
-
-    #Create all of the small matrices and then do an hstack on them
-    out = []
-
-    cdef int i = 0
-    cdef int j = 0
-    cdef int k = 0
-
-    lnrhos = np.log(rhos)
-
-    #For each eigenspectra component
-    for j, ilnrhos in enumerate(lnrhos):
-        mat = sp.dok_matrix((m , ncomp), dtype=np.float64)
-        for i in range(m):
-            mat[i,j] = R(sparams[i], params, ilnrhos)
-        out.append(mat)
-
-    return sp.vstack(out)
-
-def V22(params, rhos):
-    lnrhos = np.log(rhos)
-    cdef int ncomp = len(rhos)
-    cdef int i = 0
-
-    mat = sp.dok_matrix((ncomp, ncomp))
-    for i, ilnrhos in enumerate(lnrhos):
-        mat[i,i] = R(params, params, ilnrhos)
-
-    return mat
-
-
-
-
 
