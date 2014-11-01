a09e8098ebac4c9233b5bf0e9aab29c32c37675b
Ian Czekala <iancze@gmail.com>
2014-11-01 10:11:15 -0400

    more GP text

--- hdfcov.py
+++ hdfcov.py
@@ -3,5 +3,4 @@
 #Defines a bunch of global variables and also parses args.
 import hdfutils
 
-if hdfutils.args.cov:
-    hdfutils.estimate_covariance(hdfutils.flatchainList[0])
+hdfutils.estimate_covariance(hdfutils.flatchainList[0])
