21db2560176479dfad2768c3cc751ea0789de70d
Ian Czekala <iancze@gmail.com>
2015-01-20 05:45:19 -0500

    Typos in script

--- scripts/librarytools/create_grid.py
+++ scripts/librarytools/create_grid.py
@@ -1,14 +1,14 @@
-from StellarSpectra.grid_tools import PHOENIXGridInterface, HDF5Creator, TRES
+from Starfish.grid_tools import PHOENIXGridInterface, HDF5Creator, TRES
 
-raw_library_path = "libraries/raw/PHOENIX/"
-mygrid = grid_tools.PHOENIXGridInterface(base=raw_library_path, wl_range=[4700, 5500])
+raw_library_path = "../../libraries/raw/PHOENIX/"
+mygrid = PHOENIXGridInterface(base=raw_library_path, wl_range=[4700, 5500])
 
-out_path = "libraries/" + "PHOENIX_F.hdf5"
+out_path = "../../libraries/" + "PHOENIX_F.hdf5"
 
 instrument = TRES()
 
 # Limit the range of stellar parameters correspondi to an F star
-creator = grid_tools.HDF5Creator(mygrid, filename=out_path, instrument,
+creator = HDF5Creator(mygrid, out_path, instrument,
                         ranges={"temp":(5800, 6500), "logg":(3.5,6.0),
                         "Z":(-1.5,1.0), "alpha":(0.0,0.0)})
 
