d4dbc8288060529fa70b48b52b2d896a239ce792
Ian Czekala <iancze@gmail.com>
2015-02-16 15:05:39 -0500

    [skip ci] Priors on the emulator optimization

--- scripts/emulator/optimize_emulator.py
+++ scripts/emulator/optimize_emulator.py
@@ -2,10 +2,14 @@ import Starfish
 from Starfish import emulator
 from Starfish.covariance import Sigma
 import numpy as np
+import math
 
 # Load the PCAGrid from the location specified in the config file
 pca = emulator.PCAGrid.open()
 PhiPhi = np.linalg.inv(emulator.skinny_kron(pca.eigenspectra, pca.M))
+
+def Glnprior(x, s, r):
+    return s * np.log(r) + (s - 1.) * np.log(x) - r*x - math.lgamma(s)
 
 def lnprob(p):
     '''
@@ -22,7 +26,12 @@ def lnprob(p):
         return 1e99
 
     lambda_xi = p[0]
-    h2params = p[1:].reshape((pca.m, -1))**2
+    hparams = p[1:].reshape((pca.m, -1))
+
+    # Calculate the prior for temp, logg, and Z
+    priors = np.sum(Glnprior(hparams[:, 0], 2., 0.013)) + np.sum(Glnprior(hparams[:, 1], 2., 1.6)) + np.sum(Glnprior(hparams[:, 2], 2., 1.6))
+
+    h2params = hparams**2
     #Fold hparams into the new shape
     Sig_w = Sigma(pca.gparams, h2params)
 
@@ -32,35 +41,29 @@ def lnprob(p):
 
     central = pca.w_hat.T.dot(np.linalg.solve(C, pca.w_hat))
 
-    # s = 5.
-    # r = 5.
-    # prior_l = s * np.log(r) + (s - 1.) * np.log(ll) - r*ll - math.lgamma(s)
-    #
-    # s = 5.
-    # r = 5.
-    # prior_z = s * np.log(r) + (s - 1.) * np.log(lz) - r*lz - math.lgamma(s)
-
-    lnp = 0.5 * (pref + central + pca.M * pca.m * np.log(2. * np.pi)) #+ prior_l + prior_z
+    lnp = -0.5 * (pref + central + pca.M * pca.m * np.log(2. * np.pi)) + priors
     print(lnp)
-    return lnp
+
+    # Negate this since we are using the fmin algorithm
+    return -lnp
 
 
 def main():
 
     # Set up starting parameters and see if lnprob evaluates.
     # p will have a length of 1 + (pca.m * (1 + len(Starfish.parname)))
-    # amp = 50.0
-    # lt = 100.
-    # ll = 0.5
-    # lZ = 0.5
+    amp = 50.0
+    lt = 100.
+    ll = 0.5
+    lZ = 0.5
     #
-    # p0 = np.hstack((np.array([1., ]),
-    # np.hstack([np.array([amp, lt, ll, lZ]) for i in range(pca.m)]) ))
-    #
-    # # Set lambda_xi
-    # p0[0] = 1.0
+    p0 = np.hstack((np.array([1., ]),
+    np.hstack([np.array([amp, lt, ll, lZ]) for i in range(pca.m)]) ))
 
-    p0 = np.load("eparams.npy")
+    # # Set lambda_xi separately
+    p0[0] = 0.3
+
+    # p0 = np.load("eparams.npy")
 
     print(lnprob(p0))
 
