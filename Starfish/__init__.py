d90e012e7224148c0082a3c16676af999aeb1f9f
Miles Lucas <mdlucas@iastate.edu>
2018-12-10 00:13:42 -0600

    added automatic setup parsing for setup as well as updated readme and requirements.txt

--- Starfish/__init__.py
+++ Starfish/__init__.py
@@ -1,21 +1,26 @@
+# We first need to detect if we're being called as part of the numpy setup
+# procedure itself in a reliable manner.
+try:
+    __STARFISH_SETUP__
+except NameError:
+    __STARFISH_SETUP__ = False
+
 __version__ = '0.1'
-# Read the users config.yaml file.
-# If it doesn't exist, print a useful help message
-import os
-import warnings
 
-import yaml
+if not __STARFISH_SETUP__:
+    import os
+    import warnings
 
-from .config import Config
+    from .config import Config
 
-if os.path.exists("config.yaml"):
-    config = Config("config.yaml")
-else:
-    base_dir = os.path.dirname(__file__)
-    default = os.path.join(base_dir, "config.yaml")
-    warnings.warn("Using the default config.yaml file located at {0}. This is likely NOT what you want. Please create a similar 'config.yaml' file in your current working directory.".format(default), UserWarning)
-    config = Config(default)
+    if os.path.exists("config.yaml"):
+        config = Config("config.yaml")
+    else:
+        base_dir = os.path.dirname(__file__)
+        default = os.path.join(base_dir, "config.yaml")
+        warnings.warn("Using the default config.yaml file located at {0}. This is likely NOT what you want. Please create a similar 'config.yaml' file in your current working directory.".format(default), UserWarning)
+        config = Config(default)
 
 
 
-__all__ = ["spectrum", "model", "grid_tools", "constants", "covariance", "utils", "emulator", "samplers", "config"]
+    __all__ = ["spectrum", "model", "grid_tools", "constants", "covariance", "utils", "emulator", "samplers", "config"]
