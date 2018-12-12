I fixed so that the return_cov actually returns the emulator covariance matrix, although it is a huge data structure and takes a few seconds to calculate. This might be implemented somewhere else in the code but I haven't dug through it yet.

Starfish/emulator/emulator.py
@@ -79,12 +79,18 @@ class Emulator:
         :param return_cov: If true, will return the covariance matrix for the weights
         :type return_cov: bool
         :return: NDarray if return_cov is False, otherwise tuple of (NDarray, NDarray)
+
+        .. warning::
+            When returning the emulator covariance matrix, this is a costly operation and will return a
+            datastructure with (N_pix x N_pix) data points. For now, don't do it.
         """
         params = np.array(params)
         mu, sig = self.get_matrix(params)
         weights = self.draw_weights(mu, sig)
         if return_cov:
-            return self.reconstruct(weights), sig
+            X = self.pca.eigenspectra.T
+            C = np.linalg.multi_dot([X, sig, X.T])
+            return self.reconstruct(weights), C
         else:
             return self.reconstruct(weights)
 

Starfish/emulator/pca.py
@@ -381,10 +381,8 @@ class PCAGrid:
         :rtype: NDarray
         """
 
-        f = np.empty((self.m, self.npix))
-        for i, (pcomp, weight) in enumerate(zip(self.eigenspectra, weights)):
-            f[i, :] = pcomp * weight
-        return np.sum(f, axis=0) * self.flux_std + self.flux_mean
+        M = self.eigenspectra.T.dot(weights)
+        return M * self.flux_std + self.flux_mean
 
     def reconstruct_all(self):
         """