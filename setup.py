6049f380402b21d722c072804285f4c33aa21881
Miles Lucas <mdlucas@iastate.edu>
2019-01-04 17:56:20 -0600

    typo in setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     ],
     install_requires=['numpy',
                       'scipy',
-                      'extinction'
+                      'extinction',
                       'cython',
                       'scikit-learn',
                       'emcee',
