66feb603d1784beb0215743907275e69defa65cd
Miles Lucas <mdlucas@iastate.edu>
2019-03-01 01:01:49 -0600

    added stubs and tests for new design of spectrum model

--- Starfish/models/models.py
+++ Starfish/models/models.py
@@ -1,5 +1,7 @@
 import logging
+import warnings
 
+import numpy as np
 from scipy.linalg import cho_factor, cho_solve
 
 from Starfish.utils import calculate_dv, create_log_lam_grid
@@ -8,47 +10,110 @@ from .transforms import rotational_broaden, resample, doppler_shift, extinct, re
 
 class SpectrumModel:
 
-    def __init__(self, emulator, data):
+    def __init__(self, emulator, grid_params, data=None, vsini=None, vz=None, Av=None, logOmega=None, cheb=None):
         self.emulator = emulator
-        mask = data.masks[0].astype(bool)
-        self.wave = data.wls[0][mask]
-        self.flux = data.fls[0][mask]
-        self.sigs = data.sigmas[0][mask]
-        dv = calculate_dv(self.wave)
-        self.min_dv_wl = create_log_lam_grid(dv, self.emulator.wl.min(), self.emulator.wl.max())['wl']
-        self.bulk_fluxes = resample(self.emulator.wl, self.emulator.bulk_fluxes, self.min_dv_wl)
+
+        if data is not None:
+            mask = data.masks[0].astype(bool)
+            self.wave = data.wls[0][mask]
+            self.flux = data.fls[0][mask]
+            self.sigs = data.sigmas[0][mask]
+            dv = calculate_dv(self.wave)
+            self.min_dv_wl = create_log_lam_grid(dv, self.emulator.wl.min(), self.emulator.wl.max())['wl']
+            self.bulk_fluxes = resample(self.emulator.wl, self.emulator.bulk_fluxes, self.min_dv_wl)
+        else:
+            warnings.warn('Without providing a data spectrum the emulator will be vastly oversampled and cause much '
+                          'higher computation costs.')
+            self.wave = self.emulator.wavelength
+            self.bulk_fluxes = self.emulator.bulk_fluxes
+            self.min_dv_wl = self.wave
+
+        self.grid_params = grid_params
+        self.num_grid_params = len(grid_params)
+        self.vsini = vsini
+        self.vz = vz
+        self.Av = Av
+        self.logOmega = logOmega
+        self.cheb = cheb
+
 
         self.log = logging.getLogger(self.__class__.__name__)
 
-    def __call__(self, parameters):
+    def __call__(self):
         wave = self.min_dv_wl
-        fls = self.bulk_fluxes
+        fluxes = self.bulk_fluxes
 
-        if parameters.vsini is not None:
-            fls = rotational_broaden(wave, fls, parameters.vsini)
+        if self.vsini is not None:
+            fluxes = rotational_broaden(wave, fluxes, self.vsini)
 
-        if parameters.vz is not None:
-            wave = doppler_shift(wave, parameters.vz)
+        if self.vz is not None:
+            wave = doppler_shift(wave, self.vz)
 
-        if parameters.Av is not None:
-            fls = extinct(wave, fls, parameters.Av)
+        if self.Av is not None:
+            fluxes = extinct(wave, fluxes, self.Av)
 
-        if parameters.logOmega is not None:
-            fls = rescale(fls, parameters.logOmega)
+        if self.logOmega is not None:
+            fluxes = rescale(fluxes, self.logOmega)
 
         # Not my favorite solution because I don't like exploiting falsiness of None
-        if all(parameters.cheb):
-            fls = chebyshev_correct(wave, fls, parameters.cheb)
+        if all(self.cheb):
+            fluxes = chebyshev_correct(wave, fluxes, self.cheb)
 
-        fls = resample(wave, fls, self.wave)
-        weights, weights_cov = self.emulator(parameters.grid_params)
-        cho = cho_factor(weights_cov)
-        eigenspectra = fls[:-2]
-        flux_mean, flux_std = fls[-2:]
+        fluxes = resample(wave, fluxes, self.wave)
+        weights, weights_cov = self.emulator(self.grid_params)
+        L, flag = cho_factor(weights_cov)
+
+        # Decompose the bulk_fluxes (see emulator/emulator.py for the ordering)
+        eigenspectra = fluxes[:-2]
+        flux_mean, flux_std = fluxes[-2:]
+
+        # Complete the reconstruction
         X = eigenspectra * flux_std
-        cov = X.T @ cho_solve(cho, X)
+        cov = X.T @ cho_solve((L, flag), X)
         return weights @ X + flux_mean, cov
 
+    def get_parameter_dict(self):
+        params = {
+            'grid_params': self.grid_params,
+            'vsini': self.vsini,
+            'vz': self.vz,
+            'Av': self.Av,
+            'logOmega': self.logOmega,
+            'cheb': self.cheb
+        }
+        return params
+
+    def set_parameter_dict(self, P):
+        self.grid_params = P['grid_params']
+        self.vsini = P['vsini']
+        self.vz = P['vz']
+        self.Av = P['Av']
+        self.logOmega = P['logOmega']
+        self.cheb = P['cheb']
+
+    def get_parameter_vector(self):
+        params = self.get_parameter_dict()
+        vector = [*params['grid_params'], params['vsini'], params['vz'], params['Av'], params['logOmega'],
+                  *params['cheb']]
+        return np.array(vector)
+
+    def set_parameter_vector(self, P):
+        grid_params = P[:self.num_grid_params]
+        self.vsini, self.vz, self.Av, self.logOmega = P[self.num_grid_params:self.num_grid_params + 4]
+        self.cheb = P[self.num_grid_params + 4:]
+
+    def log_likelihood(self):
+        pass
+
+    def grad_log_likelihood(self):
+        raise NotImplementedError("If you've stumbled across this, we'd love someone to calculate this gradient!")
+
+    def save(self):
+        pass
+
+    @classmethod
+    def load(cls, filename):
+        pass
 
 class EchelleModel:
     pass
--- tests/test_models/test_models.py
+++ tests/test_models/test_models.py
@@ -1,6 +1,41 @@
+import pytest
+import numpy as np
+
+from Starfish.models import SpectrumModel
+
 class TestSpectrumModel:
 
-    def test_transform(self, mock_model, mock_parameter):
-        flux, cov = mock_model(mock_parameter)
+    def test_transform(self, mock_model):
+        flux, cov = mock_model()
         assert cov.shape == (len(flux), len(flux))
         assert flux.shape == mock_model.wave.shape
+
+    def test_warns_without_data(self, mock_emulator):
+        with pytest.warns(UserWarning):
+            model = SpectrumModel(mock_emulator, [6000, 4.0, 0])
+
+    def test_get_set_parameters(self, mock_model):
+        P0 = mock_model.get_parameter_vector()
+        mock_model.set_parameter_vector(P0)
+        P1 = mock_model.get_parameter_vector()
+        assert np.allclose(P1, P0)
+
+    def test_get_set_param_dict(self, mock_model):
+        P0 = mock_model.get_parameter_dict()
+        mock_model.set_parameter_dict(P0)
+        P1 = mock_model.get_parameter_dict()
+        assert P0 == P1
+
+    def test_log_likelihood(self, mock_model):
+        logl = mock_model.log_likelihood()
+        assert np.isfinite(logl)
+
+    def test_grad_log_likelihood(self, mock_model):
+        with pytest.raises(NotImplementedError):
+            mock_model.grad_log_likelihood()
+
+    def test_save_load(self, mock_model, tmpdir):
+        path = tmpdir.join('model.hdf5')
+        mock_model.save(path)
+        model = SpectrumModel.load(path)
+        assert model == mock_model
