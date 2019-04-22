2612be2898af871e1f83592d4a5099739955a23e
Miles Lucas <mdlucas@iastate.edu>
2019-04-21 19:15:48 -0500

    fixed setup for removed cython file

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import Starfish
 import sys
 from setuptools import setup, Extension, find_packages
 import builtins
@@ -5,46 +6,15 @@ import builtins
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-try:
-    from Cython.Distutils import build_ext
-except ImportError:
-    from setuptools.command.build_ext import build_ext
-
-    ext_modules = [Extension("Starfish.models._kernels",
-                             sources=["Starfish/emulator/_kernels.c"],
-                             extra_compile_args=["-Wno-declaration-after-statement",
-                                                 "-Wno-error=declaration-after-statement",
-                                                 "-Wno-unused-function",
-                                                 "-Wno-unused-variable",
-                                                 "-Wno-unused-but-set-variable"]),
-                   ]
-else:
-    ext_modules = [Extension("Starfish.models._kernels",
-                             sources=["Starfish/models/_kernels.pyx"],
-                             extra_compile_args=["-Wno-declaration-after-statement",
-                                                 "-Wno-error=declaration-after-statement",
-                                                 "-Wno-unused-function",
-                                                 "-Wno-unused-variable",
-                                                 "-Wno-unused-but-set-variable"]),
-                   ]
-
 if sys.version < '3.5.2':
-    raise RuntimeError('Error: Python 3.6 or greater required for Starfish (using {})'.format(sys.version))
+    raise RuntimeError(
+        'Error: Python 3.6 or greater required for Starfish (using {})'.format(sys.version))
 
 # This is a bit hackish: we are setting a global variable so that the main
 # Starfish.__init__ can detect if it is being loaded by the setup routine, to
 # avoid attempting to load components that aren't built yet.  While ugly, it's
 # a lot more robust than what was previously being used.
 builtins.__STARFISH_SETUP__ = True
-import Starfish
-
-
-# Use this custom class to be able to force numpy installation before using it.
-class CustomBuildExt(build_ext):
-    def run(self):
-        import numpy
-        self.include_dirs.append(numpy.get_include())
-        return super().run()
 
 
 setup(
@@ -78,7 +48,5 @@ setup(
                       'oyaml'],
     maintainer="Ian Czekala",
     maintainer_email="iancze@gmail.com",
-    cmdclass={'build_ext': CustomBuildExt},
-    ext_modules=ext_modules,
     include_package_data=True,
 )
