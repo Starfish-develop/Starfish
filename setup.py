a7a21cfd46027d34934be9236b9d9c4db9d68423
Miles Lucas <mdlucas@iastate.edu>
2019-05-22 14:13:39 -0500

    officially switch to python >3.6

--- setup.py
+++ setup.py
@@ -6,10 +6,6 @@ import builtins
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-if sys.version < '3.5.2':
-    raise RuntimeError(
-        'Error: Python 3.6 or greater required for Starfish (using {})'.format(sys.version))
-
 # This is a bit hackish: we are setting a global variable so that the main
 # Starfish.__init__ can detect if it is being loaded by the setup routine, to
 # avoid attempting to load components that aren't built yet.  While ugly, it's
@@ -20,6 +16,7 @@ builtins.__STARFISH_SETUP__ = True
 setup(
     name='astrostarfish',
     version=Starfish.__version__,
+    python_requires='>3.6.0',
     author='Ian Czekala',
     author_email='iczekala@berkeley.edu',
     packages=find_packages(),
