87216aafbfcc4fe66a1761eb0b667986170252f5
Miles Lucas <mdlucas@iastate.edu>
2019-06-06 20:03:56 -0500

    switch to using poetry

--- Starfish/__init__.py
+++ Starfish/__init__.py
@@ -1,24 +1,17 @@
 # We first need to detect if we're being called as part of the numpy setup
 # procedure itself in a reliable manner.
-try:
-    __STARFISH_SETUP__
-except NameError:
-    __STARFISH_SETUP__ = False
+__version__ = "0.3.0-dev"
 
-__version__ = "0.3.1-dev"
+from .spectrum import Spectrum
 
-if not __STARFISH_SETUP__:
-
-    from .spectrum import Spectrum
-
-    __all__ = [
-        "constants",
-        "emulator",
-        "grid_tools",
-        "models",
-        "samplers",
-        "spectrum",
-        "Spectrum",
-        "transforms",
-        "utils",
-    ]
+__all__ = [
+    "constants",
+    "emulator",
+    "grid_tools",
+    "models",
+    "samplers",
+    "spectrum",
+    "Spectrum",
+    "transforms",
+    "utils",
+]
